9c28d00339cbcd1ef1f5029ba6dcb3bac9125420
Wahid Niam <iam.wahidn@gmail.com>
2023-04-20 13:40:56 +0700

    feat(config): use secrecy for password

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,5 @@
 use once_cell::sync::Lazy;
+use secrecy::ExposeSecret;
 use std::{net::TcpListener};
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 use sqlx::{PgPool, Executor, PgConnection, Connection};
@@ -45,7 +46,7 @@ async fn spawn_app() -> TestApp {
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     // Create database
-    let mut connection = PgConnection::connect(&config.connection_string_without_db())
+    let mut connection = PgConnection::connect(&config.connection_string_without_db().expose_secret())
         .await
         .expect("Failed to connect to Postgres");
 
@@ -54,7 +55,7 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
         .await
         .expect("Failed to create database.");
     // Migrate database
-    let connection_pool = PgPool::connect(&config.connection_string())
+    let connection_pool = PgPool::connect(&config.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");
 
